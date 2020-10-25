63141b344c501fbd15f69164c2d3f59f56e3893a
DimitrisPatiniotis <dimpatspy@gmail.com>
2020-10-25 11:12:49 +0200

    post db model

--- blog/views.py
+++ blog/views.py
@@ -1,27 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-
-posts = [
-    {
-        'author': 'Mike Arambatzis',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'September 21, 2020'
-    },{
-        'author': 'Evelin Kinti',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'November 2, 2020'
-    }
-]
+from .models import Post
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', {'title':'About'})
